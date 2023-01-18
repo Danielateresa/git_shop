a799181ecc953d90fb19971372adfd5860eda26a
Danielateresa <dany.t_87@hotmail.it>
2023-01-18 16:39:26 +0100

    create-controller-view

---
+++ resources/views/admin/products/create.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<body>
+
+
+    <h1>Create new Products </h1>
+
+    @if (session('message'))
+    <div class="alert alert-success">
+        {{ session('message') }}
+
+    </div>
+    @endif
+
+    <form action="{{route('admin.products.store')}}" method="post">
+        @csrf
+
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror"
+                aria-describedby="helpId" value="{{ old('name') }}">
+            <small id="helpId" class="text-muted">Insert name, required field</small>
+        </div>
+        @error('name')
+        <div class="alert alert-danger">{{$message}}</div>
+        @enderror
+
+        <div class="mb-3">
+            <label for="slug" class="form-label">Slug</label>
+            <input type="text" name="slug" id="slug" class="form-control @error('slug') is-invalid @enderror"
+                aria-describedby="helpId" value="{{ old('slug') }}">
+            <small id="helpId" class="text-muted"></small>
+        </div>
+        @error('slug')
+        <div class="alert alert-danger">{{$message}}</div>
+        @enderror
+
+        <div class="mb-3">
+            <label for="description" class="form-label">Description</label>
+            <textarea name="description" id="description"
+                class="form-control @error('description') is-invalid @enderror" aria-describedby="helpId"
+                value="{{ old('description') }}"></textarea>
+            <small id="helpId" class="text-muted"></small>
+        </div>
+        @error('description')
+        <div class="alert alert-danger">{{$message}}</div>
+        @enderror
+
+        <div class="mb-3">
+            <label for="price" class="form-label">Price</label>
+            <input step="0.01" type="number" name="price" id="price"
+                class="form-control @error('price') is-invalid @enderror" aria-describedby="helpId"
+                value="{{ old('price') }}">
+            <small id="helpId" class="text-muted"></small>
+        </div>
+        @error('price')
+        <div class="alert alert-danger">{{$message}}</div>
+        @enderror
+
+        <!-- <div class="mb-3">
+            <label for="image" class="form-label">Add image</label>
+            <input type="file" name="image" id="image" class="form-control @error('image') is-invalid @enderror"
+                placeholder="" aria-describedby="helpId">
+            <small id="helpId" class="text-muted">Must be max 250kb</small>
+        </div>
+        @error('image')
+        <div class="alert alert-danger">{{$message}}</div>
+        @enderror -->
+
+        <button class="btn btn-primary" type="submit">Add Product</button>
+
+    </form>
+
+
+</body>
+@endsection
